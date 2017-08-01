c30a1269ab71e7c6c53d3a0ab1fd66b4c718d758
Oliver Sahner <osahner@medianet.de>
2017-08-01 16:05:00 +0200

    fix names

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,7 @@ gulp.task('rollup:umd', function () {
       // The name to use for the module for UMD/IIFE bundles
       // (required for bundles with exports)
       // See https://github.com/rollup/rollup/wiki/JavaScript-API#modulename
-      moduleName: 'angular-library-name',
+      moduleName: 'ngx-show-shide-password',
 
       // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals
       globals: {
@@ -139,7 +139,7 @@ gulp.task('rollup:umd', function () {
       }
 
     }))
-    .pipe(rename('angular-library-name.umd.js'))
+    .pipe(rename('ngx-show-shide-password.umd.js'))
     .pipe(gulp.dest(distFolder));
 });
 
